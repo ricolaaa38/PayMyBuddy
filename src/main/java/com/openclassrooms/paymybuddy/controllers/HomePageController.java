78dd04aea3ad3f3a47f55bffdf2ea66579e6757f
nicolas cretton <nicolas.cretton@orange.fr>
2025-06-03 23:13:27 +0200

    add Default user initializer
    add received transaction to the home table

--- src/main/java/com/openclassrooms/paymybuddy/controllers/HomePageController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controllers/HomePageController.java
@@ -37,10 +37,17 @@ public class HomePageController {
             User user = userService.getUserById(currentUserDetails.getUser().getId())
                     .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
             List<UserConnection> connections = userConnectionService.getUser1Connection(user);
-            List<Transaction> transactions = transactionService.getTransactionBySender(user);
+            List<Transaction> sentTransactions = transactionService.getTransactionBySender(user);
+            List<Transaction> receivedTransactions = transactionService.getTransactionByReceiver(user);
+
+            List<Transaction> allTransactions = new java.util.ArrayList<>();
+            allTransactions.addAll(sentTransactions);
+            allTransactions.addAll(receivedTransactions);
+            allTransactions.sort((t1, t2) -> t2.getDate().compareTo(t1.getDate()));
+
             model.addAttribute("user", user);
             model.addAttribute("connections", connections);
-            model.addAttribute("transactions", transactions);
+            model.addAttribute("transactions", allTransactions);
             model.addAttribute("currentPath", "/home");
             return "home";
     }
--- src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
@@ -1,13 +1,10 @@
 package com.openclassrooms.paymybuddy.service;
 
+
 import com.openclassrooms.paymybuddy.exception.ServiceException;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 import com.openclassrooms.paymybuddy.security.PasswordEncoderService;
-import org.apache.catalina.util.StringUtil;
-import org.apache.tomcat.util.buf.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
